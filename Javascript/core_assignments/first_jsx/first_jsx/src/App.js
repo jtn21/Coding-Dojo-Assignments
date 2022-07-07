27ab7451af91250170ffb6a1c7caa145c7fe4ca2
james nguyen <jamesnguyen04@gmail.com>
2022-07-06 23:23:46 -0700

    done?

---
+++ Javascript/core_assignments/first_jsx/first_jsx/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Hello Dojo!</h1>
+    <h2> Things I need to do:</h2>
+      <div>
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+      </div>
+    </div>
+  );
+}
+
+export default App;
