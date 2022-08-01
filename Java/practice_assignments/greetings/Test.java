13184a78ad3a939b03f4ea02b82580d2160c0b51
james nguyen <jamesnguyen04@gmail.com>
2022-07-31 21:42:00 -0700

    practice

---
+++ Java/practice_assignments/greetings/Test.java
@@ -0,0 +1,7 @@
+// file name must be same as class name
+// every file IS a class
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
