b258ab899dd435b28682b120bbde263563db22f3
james nguyen <jamesnguyen04@gmail.com>
2022-08-02 16:47:15 -0700

    finished

--- Java/practice_assignments/orderanditems/Order.java
+++ Java/practice_assignments/orderanditems/Order.java
@@ -1,6 +1,8 @@
+import java.util.ArrayList;
+
 class Order{
     public String name;
     public double total;
     public boolean ready;
-    public ArrayList<Item>items;
+    public ArrayList<String> items = new ArrayList<String>();
 }
